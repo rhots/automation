9e59860dcf03c71d2d4f013c27a85459eb0af16c
Kaleb Hermes <khermes@rocketfuel.com>
2017-01-10 16:18:43 -0500

    Used CastIronJs approach to scrape the Free Rotation.

--- bin/get_official_heroes.py
+++ bin/get_official_heroes.py
@@ -24,7 +24,7 @@ def get_heroes_data():
 
 def main():
     heroes = get_heroes_data()
-    heroes = [(h['name'], h['slug']) for h in heroes]
+    heroes = [(h['name'], h['slug']) for h in heroes if h['inFreeHeroRotation'] == True]
     print(heroes)
 
 if __name__ == "__main__":
